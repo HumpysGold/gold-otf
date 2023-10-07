264e776d79713c4270c1619e70ac44ad53a1b8a4
SHAKOTN <shakotn@protonmail.com>
2023-10-07 19:16:32 +0300

    feat: first deploy to BASE proxy

--- deploy/DeployGoldProxy.js
+++ deploy/DeployGoldProxy.js
@@ -9,14 +9,14 @@ module.exports = async function ({deployments, getNamedAccounts}) {
   console.log(`>>> your balance: ${balance.toString()}`)
   console.log(`LZ_ENDPOINTS.LZ_ADDRESS[${hre.network.name}]: " ${LZ_ENDPOINTS.LZ_ADDRESS[hre.network.name]}`);
   let goldAddress = "0xbefd5c25a59ef2c1316c5a4944931171f30cd3e4";  // GOLD on BASE
-  const lzEndpointAddress = LZ_ENDPOINTS.LZ_ADDRESS[hre.network.name]
-  // await deploy("GoldProxyOFTV2", {
-  //   from: deployer,
-  //   args: [goldAddress, 6, lzEndpointAddress],
-  //   log: true,
-  //   waitConfirmations: 1,
-  //   skipIfAlreadyDeployed: true
-  // })
+  const lzEndpointAddress = "0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7"
+  await deploy("GoldProxyOFTV2", {
+    from: deployer,
+    args: [goldAddress, 6, lzEndpointAddress],
+    log: true,
+    waitConfirmations: 1,
+    skipIfAlreadyDeployed: true
+  })
 }
 
 function getDependencies() {}

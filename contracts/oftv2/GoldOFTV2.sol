183310a799bdcab407aad2b435771e1c7c7dfa44
SHAKOTN <shakotn@protonmail.com>
2023-10-07 18:25:32 +0300

    feat: add deploy prod config

--- contracts/oftv2/GoldOFTV2.sol
+++ contracts/oftv2/GoldOFTV2.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.0;
 
 import "@layerzerolabs/solidity-examples/contracts/token/oft/v2/OFTV2.sol";
 
+// Welcome to GoldenBoys Club
+// Own it, make Yourself a GoldenBoy!
+// Its Time to Shine
+
 /// @title A LayerZero OmnichainFungibleToken example
 contract GoldOFTV2 is OFTV2 {
     constructor(string memory _name, string memory _symbol, uint8 _sharedDecimals, address _layerZeroEndpoint) OFTV2(_name, _symbol, _sharedDecimals, _layerZeroEndpoint) {}
--- deploy/DeployGoldProxy.js
+++ deploy/DeployGoldProxy.js
@@ -1,5 +1,4 @@
 const LZ_ENDPOINTS = require("@layerzerolabs/lz-sdk");
-const {ChainStage, ChainKey} = require("@layerzerolabs/lz-sdk");
 
 module.exports = async function ({deployments, getNamedAccounts}) {
   const {deploy} = deployments
@@ -9,7 +8,7 @@ module.exports = async function ({deployments, getNamedAccounts}) {
   const balance = await ethers.provider.getBalance(deployer)
   console.log(`>>> your balance: ${balance.toString()}`)
   console.log(`LZ_ENDPOINTS.LZ_ADDRESS[${hre.network.name}]: " ${LZ_ENDPOINTS.LZ_ADDRESS[hre.network.name]}`);
-  let goldAddress = "0xbefd5c25a59ef2c1316c5a4944931171f30cd3e4";
+  let goldAddress = "0xbefd5c25a59ef2c1316c5a4944931171f30cd3e4";  // GOLD on BASE
   const lzEndpointAddress = LZ_ENDPOINTS.LZ_ADDRESS[hre.network.name]
   // await deploy("GoldProxyOFTV2", {
   //   from: deployer,
